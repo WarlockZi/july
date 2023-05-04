7c95086d08bd6f1d1f07b8b40fba9420e2b457e8
WarlockZi <vvoronik@yandex.ru>
2023-05-04 23:43:37 +0300

    env fix ROOT

--- core/Connection.php
+++ core/Connection.php
@@ -4,67 +4,67 @@ namespace core;
 
 class Connection
 {
-	private $dbh;
+  private $dbh;
 
-	private $settings;
+  private $settings;
 
-	public function __construct()
-	{
-		$this->setEnv();
-		$this->connect();
-	}
+  public function __construct()
+  {
+    $this->setEnv();
+    $this->connect();
+  }
 
-	protected function setEnv()
-	{
-	  $file = dirname(__DIR__,1). '\.env';
+  protected function setEnv()
+  {
+    $file = ROOT . DIRECTORY_SEPARATOR . '.env';
 
-	  $_ENV = parse_ini_file($file);
-		$this->settings = [
-			'prefix' => 'mysql',
-			'host' => 'localhost',
-			'port' => '3306',
-			'dbname' => $_ENV['DB_NAME'],
-			'user' => $_ENV['DB_USER'],
-			'password' => $_ENV['DB_PASSWORD'],
-			'charset' => 'utf8',
-		];
-	}
+    $_ENV = parse_ini_file($file);
+    $this->settings = [
+      'prefix' => 'mysql',
+      'host' => 'localhost',
+      'port' => '3306',
+      'dbname' => $_ENV['DB_NAME'],
+      'user' => $_ENV['DB_USER'],
+      'password' => $_ENV['DB_PASSWORD'],
+      'charset' => 'utf8',
+    ];
+  }
 
-	protected function connect()
-	{
-		$dsn = "{$this->settings['prefix']}:host={$this->settings['host']};port={$this->settings['port']};dbname={$this->settings['dbname']};charset={$this->settings['charset']}";
+  protected function connect()
+  {
+    $dsn = "{$this->settings['prefix']}:host={$this->settings['host']};port={$this->settings['port']};dbname={$this->settings['dbname']};charset={$this->settings['charset']}";
 
-		$options = [
-			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-			\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
-		];
+    $options = [
+      \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+      \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+    ];
 
-		try {
-			$this->dbh = new \PDO($dsn, $this->settings['user'], $this->settings['password'], $options);
-		} catch (\PDOException $e) {
-			die('Подключение не удалось: ' . $e->getMessage());
-		}
-	}
+    try {
+      $this->dbh = new \PDO($dsn, $this->settings['user'], $this->settings['password'], $options);
+    } catch (\PDOException $e) {
+      die('Подключение не удалось: ' . $e->getMessage());
+    }
+  }
 
-	public function execute($sql, $params)
-	{
-		$sth = $this->dbh->prepare($sql);
-		$res = $sth->execute($params);
-		return $res;
-	}
+  public function execute($sql, $params)
+  {
+    $sth = $this->dbh->prepare($sql);
+    $res = $sth->execute($params);
+    return $res;
+  }
 
-	public function query($sql, $params)
-	{
-		$smt = $this->dbh->prepare($sql);
-		$result = $smt->execute($params);
-		$result = $smt->fetchAll(\PDO::FETCH_ASSOC);
+  public function query($sql, $params)
+  {
+    $smt = $this->dbh->prepare($sql);
+    $result = $smt->execute($params);
+    $result = $smt->fetchAll(\PDO::FETCH_ASSOC);
 
-		return $result ? $result : [];
-	}
+    return $result ? $result : [];
+  }
 
-	public function lastId()
-	{
-		return $this->dbh->lastInsertId();
-	}
+  public function lastId()
+  {
+    return $this->dbh->lastInsertId();
+  }
 
 }
--- model/Model.php
+++ model/Model.php
@@ -7,7 +7,6 @@ use core\Connection;
 
 class Model
 {
-	protected $di;
 	protected $db;
 	protected $table;
 
@@ -18,14 +17,6 @@ class Model
 		}
 	}
 
-	protected function exists($email, $password)
-	{
-		$instance = new self();
-		$password = Auth::encriptPassword($password);
-		$sql = "SELECT * FROM `users` WHERE `email`=? AND `password`=?";
-		$res = $instance->db->query($sql, [$email, $password]);
-		return $res ? $res[0] : null;
-	}
 
 	public static function login($email, $password)
 	{
@@ -35,47 +26,14 @@ class Model
 		if ($user) {
 			return $user;
 		}
-
 	}
 
-	public static function register($email, $password)
-	{
-		$instance = new self();
-		$user = $instance->exists($email, $password);
-		if ($user) {
-			exit(json_encode(['Пользователь с таким email уже зарегистрирован']));
-		}
-		$sql = "INSERT INTO `users` (`email`,`password`) VALUES (?,?)";
-		$password = Auth::encriptPassword($password);
-		$res = $instance->db->execute($sql, [$email, $password]);
-		$id = $instance->db->lastId();
-		if ($id) {
-			$insertedUser = self::find($id);
-		}
-		return $res ? $insertedUser : false;
-	}
-
-	public static function find($id)
-	{
-		$instance = new static();
-		$table = $instance->getTable();
-		$sql = "SELECT * FROM `{$instance->table}` WHERE `id`=?";
-		return $instance->db->query($sql, [$id])[0];
-	}
 	public static function findByNamePassword($name, $password)
 	{
 		$instance = new static();
 		$table = $instance->getTable();
 		$sql = "SELECT * FROM `{$instance->table}` WHERE `name`=? AND `password`=?";
 		return $instance->db->query($sql, [$name, $password])[0];
-	}
-
-	public static function del($id)
-	{
-		$instance = new static();
-		$table = $instance->getTable();
-		$sql = "DELETE FROM `{$table}` WHERE `id`=?";
-		return $instance->db->execute($sql, [$id]);
 	}
 
 	public static function create($arr)
